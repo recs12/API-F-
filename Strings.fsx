33e9160c1c0bc965f017c3e8342e7b2ce6b1c102
Slimane Rechdi <recs@premiertech.com>
2022-07-21 14:18:25 +0200

    operators added

--- Strings.fsx
+++ Strings.fsx
@@ -1,3 +1,6 @@
+// run with
+// dotnet fsi .\Strings.fsx
+
 open System
 
 printfn "-1 concat"
@@ -12,4 +15,25 @@ printfn "-3 exists"
 printfn "%A\n" 
     (String.exists  Char.IsDigit "6Stefan says: Hi!" )
 
+printfn "-4 filter"
+(*
+    Builds a new string containing only the characters of the input string for which the given predicate returns "true".
+*)
+printfn "%A\n" 
+    (String.filter  Char.IsDigit "6Stefan says: Hi!" )
 
+printfn "-5 forall"
+(*	
+    Tests if all characters in the string satisfy the given predicate.
+*)
+printfn "%A\n" 
+    (String.forall  Char.IsDigit "6Stefan says: Hi!" )
+
+printfn "-6 init"
+(*	
+    Builds a new string whose characters are the results 
+    of applying the function mapping to each 
+    index from 0 to count-1 and concatenating the resulting strings
+*)
+printfn "%A\n" 
+    ( String.init  3 (fun i -> (i+3).ToString()))
