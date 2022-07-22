55047c3a55e88f6575c65f5a337d67e099b3201c
Slimane Rechdi <recs@premiertech.com>
2022-07-22 21:49:38 +0200

    add operators

--- Strings.fsx
+++ Strings.fsx
@@ -37,3 +37,38 @@ printfn "-6 init"
 *)
 printfn "%A\n" 
     ( String.init  3 (fun i -> (i+3).ToString()))
+
+printfn "-7 iter"
+(*	
+    Applies the function action to each character in the string.
+*)
+printfn "%A\n" 
+    ( String.iter (fun c -> printfn "%c %d" c (int c)) "Hello")
+
+printfn "-8 iteri"
+(*	
+    Applies the function action to the index of each character in the string and the character itself.
+*)
+printfn "%A\n" 
+    ( String.iteri (fun i c -> printfn "%A %A" i c) "Hello")
+
+printfn "-9 map"
+(*	
+    Applies the function action to the index of each character in the string and the character itself.
+*)
+printfn "%A\n" 
+    ( String.map id "Hello")
+
+printfn "-10 mapi"
+(*	
+    Applies the function action to the index of each character in the string and the character itself.
+*)
+// printfn "%A\n" 
+//     ( String.mapi (fun i c -> (i,c)) "Hello")
+
+printfn "-11 replicate"
+(*	
+    Returns a string by concatenating count instances of str.
+*)
+printfn "%A\n" 
+    ( String.replicate 10 "*")
